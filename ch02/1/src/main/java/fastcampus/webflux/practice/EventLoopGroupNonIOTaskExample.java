cb1bd22ea12d5b8931187bf0df5c62e25490b18d
Seungkyu-Han <trust1204@gmail.com>
2024-08-29 21:52:26 +0900

    netty

---
+++ ch02/1/src/main/java/fastcampus/webflux/practice/EventLoopGroupNonIOTaskExample.java
@@ -0,0 +1,19 @@
+package fastcampus.webflux.practice;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EventLoopGroupNonIOTaskExample {
+    public static void main(String[] args) {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup(5);
+
+        for(int i = 0; i < 12; i++){
+            final int idx = i;
+            eventLoopGroup.execute(() -> log.info("i: {}", idx));
+        }
+
+        eventLoopGroup.shutdownGracefully();
+    }
+}
---
+++ ch02/1/src/main/java/fastcampus/webflux/practice/SampleChannelInboundHandler.java
@@ -0,0 +1,33 @@
+package fastcampus.webflux.practice;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class SampleChannelInboundHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if(msg instanceof String) {
+            ctx.writeAndFlush("Hello, " + msg)
+                    .addListener(ChannelFutureListener.CLOSE);
+        }
+        else if(msg instanceof ByteBuf) {
+            try{
+                var buf = (ByteBuf) msg;
+                var len = buf.readableBytes();
+                var charset = StandardCharsets.UTF_8;
+                var body = buf.readCharSequence(len, charset);
+                ctx.fireChannelRead(body);
+            }finally {
+                ReferenceCountUtil.release(msg);
+            }
+        }
+    }
+}
